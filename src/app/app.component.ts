2e84a6219d5ee946fe1f9deaa631a37ba8756e6d
aneenajohn <75213145+aneenajohn@users.noreply.github.com>
2021-11-22 22:32:58 +0530

    Initial commit for task tracker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// Props are component decorations
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Task-Tracker';
+  
 }
